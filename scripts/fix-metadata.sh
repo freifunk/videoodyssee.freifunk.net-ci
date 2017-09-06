2f400d24d9930acd13a51b370f710dc79d1f015a
Andreas Bräu <andreas.braeu@idealo.de>
2017-09-06 20:58:30 +0200

    extend script to use input and output file path

--- scripts/fix-metadata.sh
+++ scripts/fix-metadata.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-FILE=$1
+INPUTFILE=$1
+OUTPUTFILE=$2
 
-ffmpeg -i ${FILE} \
+ffmpeg -i ${INPUTFILE} \
   -map_metadata 0 \
   -metadata:s:a:0 LICENSE="Licensed to the public under https://creativecommons.org/licenses/by/2.0/de/ - http://media.freifunk.net" \
-  -c:a copy ${FILE}
+  -c:a copy ${OUTPUTFILE}
