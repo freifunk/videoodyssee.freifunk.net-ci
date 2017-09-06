43a3925eafb07f27b40601cb2e0f17f9ce051096
Andreas Bräu <andreas.braeu@idealo.de>
2017-09-06 22:53:00 +0200

    add script for h264 AAC HQ

---
+++ scripts/encode_h264_AAC_HQ.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+INPUT=$1
+OUTPUT=$2
+
+ffmpeg -v warning -i $INPUT -vf yadif \
+  -c:v libx264 -pix_fmt yuv420p -crf:v 14 -profile:v high -level:v 4.2 \
+  -c:a libfdk_aac -b:a 192k -aspect 16:9 -metadata title="<title>" \
+    -metadata album="<album>" \
+    -metadata copyright="Licensed to the public under http://creativecommons.org/licenses/by-sa/3.0/" \
+    -f mp4 -movflags faststart $OUTPUT
