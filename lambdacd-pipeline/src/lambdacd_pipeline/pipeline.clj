4d8fb12b7fe0ab7fd0fcda9289f9a47dda9c0361
Christian Dräger <christian.draeger1@gmail.com>
2017-11-23 22:14:31 +0100

    remove manual trigger

--- lambdacd-pipeline/src/lambdacd_pipeline/pipeline.clj
+++ lambdacd-pipeline/src/lambdacd_pipeline/pipeline.clj
@@ -2,8 +2,8 @@
   (:use [lambdacd.steps.control-flow]
         [lambdacd-pipeline.steps])
   (:require
-   [lambdacd.steps.manualtrigger :as manualtrigger]
-   [lambdacd-pipeline.trigger :as trigger])
+    [lambdacd.steps.manualtrigger :as manualtrigger]
+    [lambdacd-pipeline.trigger :as trigger])
   (:refer-clojure :exclude
                   [alias]))
 
@@ -11,34 +11,33 @@
 (def pipeline-def
   `((alias
      "triggers"
-     (either
-      manualtrigger/wait-for-manual-trigger
-      trigger/wait-for-external-trigger
-      ))
+     trigger/wait-for-external-trigger)
 
-    (alias "Processing Video"
-           (with-workspace
-            (alias "Fix Metadata"
-                   (run fix-metadata))
-            (in-parallel
-             (alias "Encode WBEM"
-                    (run encode-wbem))
-             (alias "Encode H264"
-                    (run encode-h264)))))
-    (alias "Publishing Video"
-           (with-workspace
-            (alias "Confirm publishing"
-                   manualtrigger/wait-for-manual-trigger)
-            (in-parallel
-             (alias "Upload to CDN"
-                    (run upload-to-cdn))
-             (alias "Upload to YouTube"
-                    (run upload-to-youtube)))
-            (in-parallel
-             (alias "Publish to VocToWeb"
-                    (run publish-to-voctoweb))
-             (alias "Publish to Social Media"
-                    (run publish-to-socialmedia)))
-            (alias "Cleanup processed files"
-                   (run cleanup))
-  ))))
+    (alias
+     "Processing Video"
+     (with-workspace
+      (alias "Fix Metadata"
+             (run fix-metadata))
+      (in-parallel
+       (alias "Encode WBEM"
+              (run encode-wbem))
+       (alias "Encode H264"
+              (run encode-h264)))))
+
+    (alias
+     "Publishing Video"
+     (with-workspace
+      (alias "Confirm publishing"
+             manualtrigger/wait-for-manual-trigger)
+      (in-parallel
+       (alias "Upload to CDN"
+              (run upload-to-cdn))
+       (alias "Upload to YouTube"
+              (run upload-to-youtube)))
+      (in-parallel
+       (alias "Publish to VocToWeb"
+              (run publish-to-voctoweb))
+       (alias "Publish to Social Media"
+              (run publish-to-socialmedia)))
+      (alias "Cleanup processed files"
+             (run cleanup))))))
