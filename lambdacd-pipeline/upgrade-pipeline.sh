ec28d17ace9a3f3098ca21794baaa19b2327a5ef
Christian Dräger <christian.draeger1@gmail.com>
2017-11-23 14:45:00 +0100

    activate check for uncommited changes to deploy script

--- lambdacd-pipeline/upgrade-pipeline.sh
+++ lambdacd-pipeline/upgrade-pipeline.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-#if [ -n "$(git status --porcelain)" ]; then
-#	  git status --porcelain
-#  	echo "there are changes";
-#  	echo "please commit everything so we have a reproducible git commit ID as LambdaCD version";
-#  	exit 1;
-#fi
+if [ -n "$(git status --porcelain)" ]; then
+	git status --porcelain
+	echo "there are changes";
+	echo "please commit everything so we have a reproducible git commit ID as LambdaCD version";
+	exit 1;
+fi
 
 USER=$1
 
