7cbc1e99597cf85841891324cae0897f22ae6b74
Andreas Bräu <ab@andi95.de>
2017-12-02 20:50:56 +0100

    add first script fragments for publishing to cdn

---
+++ scripts/publish_videos_at_voctoweb.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# create event
+curl -H "CONTENT-TYPE: application/json" -d '{
+    "api_key":"4",
+    "acronym":"frab123",
+    "poster_url":"http://koeln.ccc.de/images/chaosknoten_preview.jpg",
+    "thumb_url":"http://koeln.ccc.de/images/chaosknoten.jpg",
+    "event":{
+      "guid":"123",
+      "slug":"123",
+      "title":"qwerty"
+    }
+  }' "http://localhost:3000/api/events"
+
+# add recording wbem
+curl -H "CONTENT-TYPE: application/json" -d '{
+    "api_key":"4",
+    "guid":"123",
+    "recording":{
+      "filename":"some.mp4",
+      "folder":"h264-hd",
+      "mime_type":"video/mp4",
+      "language":"deu"
+      "size":"12",
+      "length":"3600"
+      }
+  }' "http://localhost:3000/api/recordings"
+
+# add recording mp4
+curl -H "CONTENT-TYPE: application/json" -d '{
+    "api_key":"4",
+    "guid":"123",
+    "recording":{
+      "filename":"some.mp4",
+      "folder":"h264-hd",
+      "mime_type":"video/mp4",
+      "language":"deu"
+      "size":"12",
+      "length":"3600"
+      }
+  }' "http://localhost:3000/api/recordings"
