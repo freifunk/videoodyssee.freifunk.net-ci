2cbf15cc418859ae8961003c38d5380e798fcfec
Andreas Bräu <ab@andi95.de>
2017-12-02 10:11:42 +0100

    set correct output filename

--- scripts/create_poster_thumbnails.sh
+++ scripts/create_poster_thumbnails.sh
@@ -2,7 +2,8 @@
 
 INPUT=$1
 OUTPUTPATH=$2
+OUTPUTFILE=$(basename $1)
 
-ffmpeg -i $INPUT -vf  "thumbnail,scale=1920:1080" -frames:v 1 -f image2pipe -vcodec png - | convert - $(OUTPUTPATH)_preview.jpg
+ffmpeg -i $INPUT -vf  "thumbnail,scale=1920:1080" -frames:v 1 -f image2pipe -vcodec png - | convert - $(OUTPUTPATH)/$(OUTPUTFILE)_preview.jpg
 
-ffmpeg -i $INPUT -vf  "thumbnail,scale=400:225" -frames:v 1 -f image2pipe -vcodec png - | convert - $(OUTPUTPATH)_thumb.jpg
+ffmpeg -i $INPUT -vf  "thumbnail,scale=400:225" -frames:v 1 -f image2pipe -vcodec png - | convert - $(OUTPUTPATH)/$(OUTPUTFILE)_thumb.jpg
