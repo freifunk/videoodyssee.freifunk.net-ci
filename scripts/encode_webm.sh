d26fb31aeb9cdf1973526f303f89d69a7c4e3dc0
Andreas Bräu <ab@andi95.de>
2018-01-11 20:40:28 +0100

    optimize file size and encoding speed

--- scripts/encode_webm.sh
+++ scripts/encode_webm.sh
@@ -8,15 +8,15 @@ PASSLOG="/tmp/${TARGET_BASE%.*}-webm-1st-pass"
 
 if [ ! -e "$PASSLOG-0.log" ]; then
 ffmpeg -analyzeduration 40000000 -probesize 100000000 -i "${SOURCE}/${TARGET_BASE}.mp4" \
-  -c:v libvpx -threads 16 -g:0 120 -b:v  1200k -qmin:0 11 -qmax:0 51 \
+  -c:v libvpx-vp9 -threads 16 -g:0 120 -b:v  1200k -qmin:0 11 -qmax:0 51 \
   -minrate:0 100k -maxrate:0 5000k \
-  -pass 1 -passlogfile  "$PASSLOG" \
+  -pass 1 -passlogfile  "$PASSLOG" -speed 4 \
   -c:a  libvorbis -b:a 96k  -ac:a 2  -ar:a 48000  -metadata:s:a language=de \
-    -f webm "${TARGET}/${TARGET_BASE}.webm"
+    -f webm /dev/null
 fi
 
 ffmpeg -y -analyzeduration  40000000  -probesize  100000000  -i "${SOURCE}/${TARGET_BASE}.mp4" \
-  -c:v  libvpx -threads 16  -pass  2  -passlogfile  "$PASSLOG" \
+  -c:v  libvpx-vp9 -threads 16  -pass  2  -passlogfile  "$PASSLOG" -speed 1 \
   -g  120  -b:v  1200k  -qmin  11  -qmax  51  -minrate  100k  -maxrate  5000k \
   -c:a  libvorbis  -b:a  96k  -ac:a 2  -ar:a 48000  \
   -f  webm "${TARGET}/${TARGET_BASE}.webm"
